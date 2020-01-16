03a29550e261f52f6117e1c0163850c89df4df80
LizaMetla <liza.metla@gmail.com>
2020-01-16 18:37:58 +0300

    trying to use if-else

--- lession_2.playground/Contents.swift
+++ lession_2.playground/Contents.swift
@@ -1,12 +1,33 @@
 //: A UIKit based Playground for presenting user interface
   
 import UIKit
-import PlaygroundSupport
 
-let constMaxUInt: UInt = UInt.max
-constMaxUInt
+//if-else
+let a = 22
+let b = 56
 
-print(constMaxUInt)
+if a>b {
+    print("that's impossible")
+} else {
+    print("that's it! a<b")
+}
+
+var c = a
+let d = 11
+
+if d>a{
+    print("impossible")
+}else if b<a{
+    print("also impossible")
+}else {
+    print("you need to know thats d<c=a<b")
+}
+
+//В данном случае num3 будет равно 4, так как num1 больше num2, поэтому будет выполняться второй операнд: num1 - num2.
+var num1 = 10
+var num2 = 6
+var num3 = num1 > num2 ? num1 - num2 : num1 + num2
+print(num3)
 
 
 //class Pl {
