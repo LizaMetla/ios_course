2a153669e603d27f8dddd35cbea2c00d3993136e
LizaMetla <liza.metla@gmail.com>
2020-01-21 21:45:25 +0300

    clusses in lesson4

--- lesson4.playground/Contents.swift
+++ lesson4.playground/Contents.swift
@@ -83,8 +83,99 @@ for (key, value) in dict {
 
 //creating SET
 //can have only unique elements
-var customSet: Set<Int> = [1, 2 ,3]
+var customSet: Set<Int> = [1, 2 ,3, 4, 5]
 customSet.count
-var subSet: Set<Int> = [2, 3, 4]
+var subSet: Set<Int> = [2, 3, 4, 7]
 var isSubset = subSet.isSubset(of: customSet)
+
+//использование замыканий, чтобы функцию передавать в функцию
+//не статическая переменная, а динамическая , как функция , в которую можно передавать
+
+var filterCustom = customSet.filter { argument -> Bool in
+    if argument < 4{
+    return true
+    } else {
+        return false
+    }
+}
+
+let newset = filterCustom.map {
+    $0 * 2
+}
+
+func newFunc (param: Int) -> Int {
+   return 1
+}
+
+newset
+filterCustom
+filterCustom.sorted()
+
+//nothing to return
+func newFunc1 (param: Int) -> Void {
+   
+}
+
+func funcName (id index: Int, param2: Double) -> Int {
+    return index
+}
+
+func color (_ red: Int? = 1, _ green: Int? = 2, _ blue: Int? = 3) -> Int?{
+    guard let red = red,
+        let green = green,
+        let blue = blue else {
+            return 0
+    }
+    return red + green + blue
+}
+
+
+var colorComponent:Int? = nil
+let variable = funcName(id: 1, param2: 1.5)
+color()
+
+
+///////////////////////////////////////////////
+struct CustomStruct {
+    func shortAction() {
+        
+    }
+    static func iternalFunc() {
+        //shortAction()
+    }
+}
+
+var customStructVar = CustomStruct()
+customStructVar.shortAction()
+CustomStruct().shortAction()
+
+var color = UIColor(white: 255, alpha: 1.0)
+
+
+//CLOSURES - замкания
+
+//typealias
+typealias MyC = (Int, String) -> Void
+
+var customClosures: (Int, String) -> Void = {integeeer,
+    striiiiing in
+    print("closures are called")
+    print(integeeer)
+    print(striiiiing)
+}
+var  newClosures = customClosures
+newClosures(1, "HELOOOOOO")
+
+//if you do not want to use argument
+var customClosures1: (Int, String) -> Void = {
+    print("closures are called")
+    print($0)
+    print($1)
+}
+
+var oneMoreClosure: MyC = {
+    $1
+    $0
+}
+
 
